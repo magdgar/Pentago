276e06a39d953a6da6c2b9a826068896840ed465
magdgar <magdgar@gmail.com>
2015-11-30 19:26:30 +0100

    some nice changes, more like android code

--- app/src/main/java/com/example/makda/pentago/BoardView.java
+++ app/src/main/java/com/example/makda/pentago/BoardView.java
@@ -4,70 +4,112 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.support.v4.util.Pair;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
 import java.util.LinkedList;
 
-public class BoardView extends View {
-    LinkedList<RectangleSegmentView> listOfSegments;
-    LinkedList<Rectangle> rectangles;
-    Context context;
+public class BoardView extends ViewGroup {
+    private QuarterBoard[] quarterBoards;
+    private int[][] permutations;
+    QuarterBoard quarterBoard;
     private int countRectClick;
-
+    private boolean wasPreviousActionClick;
+    int touchX;
 
     public BoardView(Context context) {
         super(context);
-        listOfSegments = new LinkedList<>();
-        rectangles =  new LinkedList<>();
-
-        this.context = context;
+        addQuaterBoards();
+        quarterBoard = getClickedQuarterBoard(0, 0);
+        createPermutation();
+        wasPreviousActionClick = false;
         countRectClick = 0;
-        createGrid();
     }
 
-    public void createGrid(){
-        RectangleSegmentView rsw;
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        addQuaterBoards();
+        createPermutation();
+        countRectClick = 0;
+    }
 
-        for(int i=30; i<250; i = i+210 )
-            for(int j=150; j<370; j = j+210) {
-                rsw = new RectangleSegmentView(context, new Pair(i, j));
-                rectangles.addAll(0, rsw.rectangles);
-                listOfSegments.add(rsw);
-            }
+    private void addQuaterBoards(){
+        quarterBoards = new QuarterBoard[]{new QuarterBoard(getContext()), new QuarterBoard(getContext()),
+                new QuarterBoard(getContext()), new QuarterBoard(getContext())};
+        for(QuarterBoard quarterBoard : quarterBoards)
+            addView(quarterBoard);
+    }
+
+    private void createPermutation(){
+        permutations = new int[][]{
+                {0,1,2,3,4,5,6,7,8},
+                {6,3,0,7,4,1,8,5,2},
+                {8,7,6,5,4,3,2,1,0},
+                {2,5,8,1,4,7,0,3,6}};
     }
 
     @Override
-     public void draw(Canvas canvas) {
-        for(RectangleSegmentView r : listOfSegments)
-            r.displayGrid(canvas);
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        quarterBoards[0].layout(0, 145, r / 2 - 2, b / 2);
+        quarterBoards[1].layout(r / 2, 145, r - 2, b / 2);
+        quarterBoards[2].layout(0, b / 2, r / 2 -2, b - 145);
+        quarterBoards[3].layout(r / 2, b / 2, r - 2, b - 145);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int touchX = Math.round(event.getX());
-        int touchY = Math.round(event.getY());
+        int touchY;
         int e = event.getAction();
 
         switch(e){
             case MotionEvent.ACTION_DOWN:
-                for(Rectangle rect : rectangles)
-                    checkIfRectangleMatched(rect, touchX, touchY);
+                touchX = Math.round(event.getX());
+                touchY = Math.round(event.getY());
+                checkIfRectangleMatched(touchX, touchY);
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                Log.v("asdasd", "Sliding your finger around on the screen.");
+                int x = Math.round(event.getX());
+                int rotation;
+                if(wasPreviousActionClick)
+                    if( x != touchX) {
+                        Log.d("sth", String.valueOf(x));
+                        rotation = (x - touchX > 0) ? 90 : -90;
+                        quarterBoard.setRotation(quarterBoard.getRotation() + rotation);
+                        quarterBoard.invalidate();
+                        wasPreviousActionClick = false;
+                    }
                 break;
         }
         return true;
     }
 
-    private void checkIfRectangleMatched(Rectangle rect, int touchX, int touchY){
-        if(rect.contains(touchX, touchY)){
-            changeRectColor(rect);
-            rect.isSelected = true;
-            invalidate();
+    private void checkIfRectangleMatched(int touchX, int touchY){
+        quarterBoard = getClickedQuarterBoard(touchX, touchY);
+        touchY -= quarterBoard.getTop();
+        touchX -= quarterBoard.getLeft();
+        for(Rectangle rect :quarterBoard.rectangles) {
+            if (rect.contains(touchX, touchY)) {
+                int properIndex = permutations[quarterBoard.getPermutationID()][quarterBoard.rectangles.indexOf(rect)];
+                Log.d("index", String.valueOf(quarterBoard.rectangles.indexOf(rect)));
+                Log.d("permutation id", String.valueOf(quarterBoard.getPermutationID()));
+                rect = quarterBoard.rectangles.get(properIndex);
+                changeRectColor(rect);
+                rect.isSelected = true;
+                quarterBoard.invalidate();
+            }
         }
+        wasPreviousActionClick = true;
+    }
+
+    private QuarterBoard getClickedQuarterBoard(int touchX, int touchY){
+        if(touchY < getHeight()/2)
+            return (touchX < getWidth()/2)? quarterBoards[0] : quarterBoards[1];
+        else
+            return (touchX < getWidth()/2)? quarterBoards[2] : quarterBoards[3];
     }
 
     private void changeRectColor(Rectangle rect){
--- app/src/main/java/com/example/makda/pentago/Rectangle.java
+++ app/src/main/java/com/example/makda/pentago/Rectangle.java
@@ -1,36 +1,25 @@
 package com.example.makda.pentago;
 
-import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.support.v4.util.Pair;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
-public class Rectangle extends View{
+public class Rectangle{
 
     Rect rect;
-    private Paint paint;
+    Paint paint;
     boolean isSelected;
 
-    public Rectangle(Context context, Rect rect){
-        super(context);
+    public Rectangle(Rect rect){
         this.rect = rect;
         paint = new Paint();
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.GRAY);
         isSelected = false;
-    }
-
-    public void draw(Canvas canvas){
-        canvas.drawRect(this.rect, paint);
     }
 
     public boolean contains(int x, int y){
         if(x > rect.left && x < rect.right)
-            if(y > rect.top && y < rect.bottom)
+            if (y > rect.top && y < rect.bottom)
                 return true;
         return false;
     }
