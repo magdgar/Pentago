f807a32481f7b1fad3b845d999ce7edbdca6cb76
magdgar <magdgar@gmail.com>
2015-12-04 10:41:14 +0100

    rotation bug fix

--- app/src/main/java/com/example/makda/pentago/QuarterBoard.java
+++ app/src/main/java/com/example/makda/pentago/QuarterBoard.java
@@ -38,7 +38,7 @@ public class QuarterBoard extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(/*new Random().nextInt()*/ Color.GREEN);
+        canvas.drawColor(/*new Random().nextInt()*/ Color.WHITE);
         for( Rectangle r : rectangles)
             canvas.drawRect(r.rect, r.paint);
     }
@@ -48,11 +48,11 @@ public class QuarterBoard extends View {
         if( r == 0)
             return 0;
         if(r == 90 || r == -270)
-            return 1;
+            return 3;
         if(r == 180 || r == -180)
             return 2;
         if(r == 270 || r == -90)
-            return 3;
+            return 1;
         return 0;
     }
 }
