dc59d2590b68f453e172d2a0824b9e4fe09e6c27
magdgar <magdgar@gmail.com>
2016-01-09 16:53:49 +0100

    my first triangle in OpenGL

--- app/src/main/java/com/example/makda/pentago/boards/triangle/triangle/MyGLRenderer.java
+++ app/src/main/java/com/example/makda/pentago/boards/triangle/triangle/MyGLRenderer.java
@@ -31,15 +31,18 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         mTriangle = new Triangle();
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
         mTriangle.draw();
     }
 
     public void onDrawFrame(GL10 unused) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         mTriangle.draw();
     }
 }
